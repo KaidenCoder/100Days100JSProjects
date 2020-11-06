0bfbe1aaf99313890f2aeadfb591a3573f861bd7
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-11-06 22:20:07 +0530

    secreviews

---
+++ secondreviews/src/data.js
@@ -0,0 +1,22 @@
+const secreviews = [
+    {
+        id: 1,
+        name: 'kurosaki',
+        text: "Ask, and it will be given to you; seek, and you will find; knock, and it will be opened to you",
+        date: '6th Nov, 2020'
+    },
+    {
+        id: 2,
+        name: 'naruto uzumaki',
+        text: "Therefore do not be anxious about tomorrow, for tomorrow will be anxious for itself. Sufficient for the day is its own trouble.",
+        date: '5th Nov, 2020'
+    },
+    {
+        id: 3,
+        name: 'luffy',
+        text: "For the gate is narrow and the way is hard that leads to life, and those who find it are few.",
+        date: '4th Nov, 2020'
+    },
+]
+
+export default secreviews
